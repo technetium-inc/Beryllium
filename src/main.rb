be135b7f3ae345aaa86f2bc83c62d9e494b8467b
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-03-01 18:24:05 +0000

    Do lexer

---
+++ src/main.rb
@@ -0,0 +1,17 @@
+def readFile
+    file = File.open(ARGV[0])
+    file.read()
+end
+
+def lex code
+    code.split
+end
+
+def execute tokens
+end
+
+def main
+    execute lex readFile 
+end
+
+main
